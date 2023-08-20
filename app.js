ea0b514fa86965462f402851455c50aba488391c
penpenpengin <pengin15081997@gmail.com>
2023-08-20 09:55:07 +0200

    Infinite Saker

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let sliderWrap = document.querySelector('.slider-wrap');
 let slider = document.querySelector('.slider');
 
-let clonesWidth;
-let sliderWidth;
+let clonesHeight;
+let sliderHeight;
 let clones = [];
 let disableScroll = false;
 let scrollPos;
@@ -11,7 +11,7 @@ let items = [...document.querySelectorAll('.slider-item')];
 let images = [...document.querySelectorAll('.img-div')];
 
 images.forEach((image, index) => {
-    image.style.backgroundImage = `url(pengin.png)`;
+    image.style.backgroundImage = `url(saker.png)`;
 });
 
 items.forEach((item) => {
@@ -21,12 +21,12 @@ items.forEach((item) => {
     clones.push(clone);
 });
 
-function getClonesWidth() {
-    let width = 0;
+function getClonesHeight() {
+    let height = 0;
     clones.forEach((clone) => {
-        width += clone.offsetWidth;
+        height += clone.offsetHeight;
     });
-    return width;
+    return height;
 }
 
 function getScrollPos() {
@@ -39,26 +39,26 @@ function setScrollPos(pos) {
 
 function scrollUpdate() {
     scrollPos = getScrollPos();
-    if (clonesWidth + scrollPos >= sliderWidth) {
+    if (clonesHeight + scrollPos >= sliderHeight) {
         setScrollPos(1);
     } else if (scrollPos <= 0) {
-        setScrollPos(sliderWidth - clonesWidth - 1);
+        setScrollPos(sliderHeight - clonesHeight - 1);
     }
 
-    slider.style.transform = `translateX(${-scrollPos}px)`;
+    slider.style.transform = `translateY(${-scrollPos}px)`;
     requestAnimationFrame(scrollUpdate);
 }
 
 function onLoad() {
     calculateDimensions();
-    document.body.style.height = `${sliderWidth}px`;
+    document.body.style.height = `${sliderHeight}px`;
     setScrollPos(1);
     scrollUpdate();
 }
 
 function calculateDimensions() {
-    sliderWidth = slider.getBoundingClientRect().width;
-    clonesWidth = getClonesWidth();
+    sliderHeight = slider.getBoundingClientRect().width;
+    clonesHeight = getClonesHeight();
 }
 
 onLoad();
