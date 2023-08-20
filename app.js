f8ba3c82304b4a4b0489e63ec5bb0cf513d2ec8c
penpenpengin <pengin15081997@gmail.com>
2023-08-20 09:56:14 +0200

    New Infinite Saker

--- app.js
+++ app.js
@@ -1,64 +1,37 @@
-let sliderWrap = document.querySelector('.slider-wrap');
-let slider = document.querySelector('.slider');
 
-let clonesHeight;
-let sliderHeight;
-let clones = [];
-let disableScroll = false;
-let scrollPos;
+let container = document.querySelector('.container');
+let images = [...document.querySelectorAll('.img')];
+let h0 = container.attributes.getNamedItem('height');
 
-let items = [...document.querySelectorAll('.slider-item')];
-let images = [...document.querySelectorAll('.img-div')];
-
-images.forEach((image, index) => {
-    image.style.backgroundImage = `url(saker.png)`;
+let vals = new Array(images.length).fill(0).map(_ => Math.round(Math.random()));
+vals[0] = 0;
+images.forEach((image, idx) => {
+    if(idx >= 1 && vals[idx - 1] == 1) { vals[idx] = 0; }
+    image.style.backgroundImage = `url(saker-${vals[idx]}.png)`;
 });
 
-items.forEach((item) => {
-    let clone = item.cloneNode(true);
-    clone.classList.add('clone');
-    slider.appendChild(clone);
-    clones.push(clone);
-});
-
-function getClonesHeight() {
-    let height = 0;
-    clones.forEach((clone) => {
-        height += clone.offsetHeight;
-    });
-    return height;
-}
-
-function getScrollPos() {
-    return window.scrollY;
-}
-
-function setScrollPos(pos) {
-    window.scrollTo({top: pos});
+function addPanel() {
+    let panel = document.createElement('div');
+    panel.classList.add('img');
+    
+    if (vals[vals.length - 1] == 1) { vals.push(0); }
+    else { vals.push(Math.round(Math.random())); }
+    panel.style.backgroundImage = `url(saker-${vals[vals.length - 1]}.png)`;
+    
+    container.appendChild(panel);
 }
 
 function scrollUpdate() {
-    scrollPos = getScrollPos();
-    if (clonesHeight + scrollPos >= sliderHeight) {
-        setScrollPos(1);
-    } else if (scrollPos <= 0) {
-        setScrollPos(sliderHeight - clonesHeight - 1);
+    scrollPos = window.scrollY + window.innerHeight;
+    if (scrollPos > document.body.offsetHeight) {
+        addPanel();
     }
-
-    slider.style.transform = `translateY(${-scrollPos}px)`;
+    
     requestAnimationFrame(scrollUpdate);
 }
 
 function onLoad() {
-    calculateDimensions();
-    document.body.style.height = `${sliderHeight}px`;
-    setScrollPos(1);
     scrollUpdate();
-}
-
-function calculateDimensions() {
-    sliderHeight = slider.getBoundingClientRect().width;
-    clonesHeight = getClonesHeight();
 }
 
 onLoad();
